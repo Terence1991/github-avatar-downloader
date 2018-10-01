eae0b5571b9d9c8b72f8b8d9a56a59cfb531fb58
Terence1991 <cplterencehawes@gmail.com>
2018-10-01 23:20:54 +0000

    function implemented

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var request2 = require('./secret.js')
+var fs = require('fs');
 var myToken = request2.Terence1991;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -24,5 +25,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
     var avatarURL =  output.map(a => a.avatar_url)
 
     console.log(avatarURL);
+    
   });
+
   
+  function downloadImageByURL(url, filePath) {
+    // ...
+    request.get(url)
+    .on('error', function(err) {
+        throw err;
+    })
+    .on('response', function(response) {
+        console.log('Response status code:', response.statusCode);
+    })
+    .on("end", function() {
+        console.log('downloading image...')
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function() {
+     console.log("Download complete");
+    });
+  }
+
+  downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
